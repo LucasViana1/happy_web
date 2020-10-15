7a868deea866f153ec2de803b12378898092a9d7
Lucas Cunha <lucasviana112@gmail.com>
2020-10-15 01:00:18 -0300

    created routes for create and view orphanage

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,17 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import CreateOrphanage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path='/' component={Landing} exact />
       <Route path='/app' component={OrphanagesMap} />
+
+      <Route path='/orphanages/create' component={CreateOrphanage} />
+      <Route path='/orphanages/:id' component={Orphanage} />
     </Switch>
   </BrowserRouter>
 );
